a544835ddae410bc0a2c86202b5fef3a3612bb47
DESKTOP-DR6T02S\saehe <saehej.kang@gmail.com>
2021-04-14 11:57:43 -0700

    added print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Jose Ortega has been at Cal Poly for almost 4 years");
         System.out.println("Micah was here"); 
+        System.out.println("Saehej was here");
     }
 }
