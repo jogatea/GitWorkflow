d1581d0c1c20a8e6484fe7a0b8af12a094a98931
Micah_Wibowo <wibowo@calpoly.edu>
2021-04-14 11:51:42 -0700

    added print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Jose Ortega has been at Cal Poly for almost 4 years"); 
+        System.out.println("Jose Ortega has been at Cal Poly for almost 4 years");
+        System.out.println("Micah was here"); 
     }
 }
